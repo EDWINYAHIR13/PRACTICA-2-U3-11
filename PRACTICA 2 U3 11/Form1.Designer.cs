6cd1a2fc310426ad09e2d27ae2dd3054e9b16414
tiago <tiago@DESKTOP-00HI8IK>
2023-11-03 21:35:57 -0500

    Agregar archivos de proyecto.

---
+++ PRACTICA 2 U3 11/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PRACTICA_2_U3_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(270, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(199, 119);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(191, 183);
+            label1.Name = "label1";
+            label1.Size = new Size(23, 24);
+            label1.TabIndex = 1;
+            label1.Text = "X";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(427, 183);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 24);
+            label2.TabIndex = 2;
+            label2.Text = "f(x)";
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 24;
+            listBox1.Location = new Point(126, 226);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 172);
+            listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            listBox2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 24;
+            listBox2.Location = new Point(367, 226);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(150, 172);
+            listBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Khaki;
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(592, 110);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 36);
+            button1.TabIndex = 5;
+            button1.Text = "for";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LemonChiffon;
+            button2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(592, 176);
+            button2.Name = "button2";
+            button2.Size = new Size(110, 38);
+            button2.TabIndex = 6;
+            button2.Text = "while";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.PaleGoldenrod;
+            button3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(592, 244);
+            button3.Name = "button3";
+            button3.Size = new Size(110, 41);
+            button3.TabIndex = 7;
+            button3.Text = "do..while";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Beige;
+            button4.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(592, 321);
+            button4.Name = "button4";
+            button4.Size = new Size(110, 39);
+            button4.TabIndex = 8;
+            button4.Text = "Salir";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
